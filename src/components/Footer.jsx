6ff5dce539cd039a8ed6d14aef81b076a5e8efa8
Andrés Morales <amoralesma@mora-macbookpro.local>
2023-02-03 21:32:54 -0500

    [Config] Update for github pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'; 
+
+export const Footer = () => {
+    return (
+        <footer className='myFooter bg-dark fluid-container p-2 w-100'>
+            Miauwricio 2023
+        </footer>
+    )
+}
