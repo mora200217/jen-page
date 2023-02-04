4cc06984713e9ab15de7ca43115a4686ecefcc00
Andrés Morales <amoralesma@mora-macbookpro.local>
2023-02-03 21:34:53 -0500

    [Deploy]

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css'; 
-
-export const Footer = () => {
-    return (
-        <footer className='myFooter bg-dark fluid-container p-2 w-100'>
-            Miauwricio 2023
-        </footer>
-    )
-}
